47fa477fab4b0b83a80f0d1d431021798e7e21f9
Nirmal Vignesh <nirmalvignesh405@gmail.com>
2024-12-14 14:30:30 +0530

    login

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 import os
-from pymongo import MongoClient
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
@@ -10,24 +8,19 @@ app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 # Set up Flask-SocketIO
 socketio = SocketIO(app)
 
-@app.route('/index')
-def login():
-    if 'username' in session:
-        return render_template('login.html')
-    return redirect(url_for('login'))
-
 @app.route('/')
 def index():
+    # Serve the index.html template
     return render_template('index.html')
-def home():
-    print("Home route accessed")
-    return redirect('/welcome')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
 
 @app.route('/welcome')
 def welcome():
-    print("Welcome route accessed")
+    # Serve a welcome message
     return "Welcome to the Website!"
-
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
